4d0aedf11fd6512344b2110abd35903e2ff0861e
Linus045 <lwillms88@gmail.com>
2022-03-10 15:23:54 +0100

    Added icon for safeeyes

---
+++ polybar/.config/polybar/scripts/safeeyes_status_time.sh
@@ -0,0 +1,10 @@
+S="$(safeeyes --status)";
+if echo $S | grep -q Disabled; then 
+  echo " Off"; 
+else
+  Time=$( echo $S | awk '{print $4}');
+  TimeInUTC=$(date -u +%H:%M -d $(date +@%s -d $Time));
+  echo "" $(datediff time -i"%H:%M" $TimeInUTC -f"%M:%0S");
+  #echo "TEST";
+fi
+
