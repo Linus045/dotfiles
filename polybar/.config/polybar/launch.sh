08f3bcb31d1c489edb79d8d9a33bea636c2b7b7f
Linus045 <lwillms88@gmail.com>
2022-04-24 18:02:06 +0200

    Make sure polybar gets killed on reload

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 # Terminate already running bars
-killall -q polybar
+killall -q --signal KILL polybar
 
 # Wait until bars have been terminated
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
