296642a6c425c241273f6d36efd95b234b7a10dc
Linus045 <lwillms88@gmail.com>
2022-04-30 15:59:25 +0200

    Automatically enable connected screens using xrandr

---
+++ polybar/.config/polybar/scripts/multi_screen_selector.sh
@@ -0,0 +1,23 @@
+disconnected_screens=$(xrandr | grep ' disconnected' | awk -F' ' '{ print $1 }')
+disconnected_screens=($(echo $disconnected_screens | tr ' ' ' '))
+
+connected_screens=$(xrandr | grep ' connected' | awk -F' ' '{ print $1 }')
+connected_screens=($(echo $connected_screens | tr ' ' ' '))
+
+
+echo "connected_screens"
+l=${#connected_screens[@]}
+for (( i=1; i<${l}; i++ )); do
+  echo ${connected_screens[i]}
+  xrandr --output ${connected_screens[i]} --auto --left-of ${connected_screens[0]}
+done
+echo ""
+
+echo "disconnected_screens"
+l=${#disconnected_screens[@]}
+for (( i=0; i<${l}; i++ )); do
+  echo ${disconnected_screens[i]}
+  xrandr --output ${disconnected_screens[i]} --off
+done
+
+
