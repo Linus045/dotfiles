1653a905a93e90fc0bb75631edac90e25f9bc62f
Linus045 <lwillms88@gmail.com>
2023-05-23 20:07:09 +0200

    Updated zsh-exports
    + added sourcing of zsh-exports.local file

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,22 +10,22 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # polybar top &)
 # i3 when i3 is used or xfce depending on the current window manager
 #echo $XDG_CURRENT_DESKTOP
+# $XDG_CURRENT_DESKTOP is set by sddm i believe
 I3ENABLED=0;
 if [ "i3" = "$XDG_CURRENT_DESKTOP" ]; then
   I3ENABLED=1
 fi
 
-echo "$I3ENABLED"
 # Manage multiple monitors
 for m in $(polybar --list-monitors | cut -d":" -f1); do
   # use top_i3 bar if i3 is used (it has other logout config and stuff)
   if [ $I3ENABLED = 1 ]; then
-    if [ $m == "eDP" ]; then
-      MONITOR=$m polybar --reload top_i3_main 2> ~/.polybar_errorlog_$m &
-    else
-      MONITOR=$m polybar --reload top_i3 2> ~/.polybar_errorlog_$m &
-    fi
-  else
-    MONITOR=$m polybar --reload top_xfce &
+	if [ $m == "eDP" ]; then
+	  MONITOR=$m polybar --reload top_i3_main 2> ~/.polybar_errorlog_$m &
+	elif [ $m == "HDMI-0" ]; then
+	  MONITOR=$m polybar --reload single_monitor
+	else
+	  MONITOR=$m polybar --reload top_i3 2> ~/.polybar_errorlog_$m &
+	fi
   fi
 done
