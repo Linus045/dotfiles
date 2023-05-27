8890ea8c0e7f3b126d5e499eec759e05b1fb394b
Linus045 <lwillms88@gmail.com>
2023-05-27 14:03:39 +0200

    (polybar) Fixed safeeyes

--- polybar/.config/polybar/scripts/safeeyes_status_time.sh
+++ polybar/.config/polybar/scripts/safeeyes_status_time.sh
@@ -1,12 +1,11 @@
 S="$(safeeyes --status)";
 if echo $S | grep -q Disabled; then
-  echo " Off";
+  echo " Idle";
 elif echo $S | grep -q "not running"; then
   echo " Off";
 else
-  Time=$( echo $S | awk '{print $4}');
+  Time=$( echo $S | awk '{print $4$5}');
   TimeInUTC=$(date -u +%H:%M -d $(date +@%s -d $Time));
   echo "" $(datediff time -i"%H:%M" $TimeInUTC -f"%M:%0S");
-  #echo "TEST";
 fi
 
