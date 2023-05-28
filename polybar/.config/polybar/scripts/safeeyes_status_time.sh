2e7235be9922dcc554d28af52f1fe623c6da4561
Linus045 <lwillms88@gmail.com>
2023-05-29 00:55:53 +0200

    Fixes mute keybind $mod+m, updates polybar for tower pc, fixes safeeyes integration

--- polybar/.config/polybar/scripts/safeeyes_status_time.sh
+++ polybar/.config/polybar/scripts/safeeyes_status_time.sh
@@ -4,8 +4,10 @@ if echo $S | grep -q Disabled; then
 elif echo $S | grep -q "not running"; then
   echo " Off";
 else
-  Time=$( echo $S | awk '{print $4$5}');
-  TimeInUTC=$(date -u +%H:%M -d $(date +@%s -d $Time));
+  # safeeyes --status uses a non-breaking space (U+202F) as a separator (why?)
+  # so we need to remove it before passing the time to date
+  Time=$( echo $S | awk '{print $4,$5}' | tr -d $(echo $'\u202F') )
+  TimeInUTC=$(date -u +%H:%M -d $(date +@%s -d $Time))
   echo "" $(datediff time -i"%H:%M" $TimeInUTC -f"%M:%0S");
 fi
 
