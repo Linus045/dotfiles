6582176429fc56c1fddbb74cfe3f874033bb6a17
Linus045 <lwillms88@gmail.com>
2023-10-05 12:43:42 +0200

    Reworks how project specific configuration is loaded
    + Moved configuration to a local git-ignored file

--- nvim/.config/nvim/lua/load_project_config.lua
+++ nvim/.config/nvim/lua/load_project_config.lua
@@ -1,14 +1,20 @@
 local M = {}
 
 
+
 M.setup = function()
-    local cwd = vim.fn.getcwd()
-    -- print(cwd)
-    if cwd == "/home/linus/dev/github/MatchBuddy" then
-        vim.opt.expandtab = true
-    elseif cwd == "/home/linus/dev/github/WORKTREE/main" then
-        vim.opt.expandtab = true
-    end
+	local cwd = vim.fn.getcwd()
+
+	local status_ok, project_configs = pcall(require, 'project_configs.local')
+	if not status_ok then
+		return
+	end
+
+	for path, config_func in pairs(project_configs.configs) do
+		if path == cwd then
+			config_func()
+		end
+	end
 end
 
 
