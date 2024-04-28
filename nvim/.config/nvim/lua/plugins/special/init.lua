bd769046aa549d076590fe7ddbcb7a01d2834ebd
Linus045 <lwillms88@gmail.com>
2024-04-28 16:05:40 +0200

    Fixes vimwiki startpage

--- nvim/.config/nvim/lua/plugins/special/init.lua
+++ nvim/.config/nvim/lua/plugins/special/init.lua
@@ -34,9 +34,17 @@ return {
 	-- vim wiki
 	{
 		"vimwiki/vimwiki",
-		config = function()
-			vim.cmd(
-				"let g:vimwiki_list = [{'path': '~/.nvim_journal/', 'path_html': '~/.nvim_journal/wiki_html/', 'index': '.nvim_journal', 'syntax': 'markdown', 'ext': '.md'}]")
+		-- using init here instead of config to set the variable before loading the plugin
+		init = function()
+			vim.g.vimwiki_list = {
+				{
+					path = "~/.nvim_journal/",
+					path_html = "~/.nvim_journal/wiki_html/",
+					index = ".nvim_journal",
+					syntax = "markdown",
+					ext = ".md"
+				}
+			}
 		end
 	},
 
