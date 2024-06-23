66e0e0ebbe1a3f76821f9c6354130f1ca2ffac1f
Linus045 <lwillms88@gmail.com>
2024-06-23 19:05:16 +0200

    feat(nvim): enables inlay hints

--- nvim/.config/nvim/lua/plugins/lsp/mason_nvim.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason_nvim.lua
@@ -273,5 +273,7 @@ return {
 		vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
 			border = "rounded",
 		})
+
+		vim.lsp.inlay_hint.enable(true);
 	end
 }
