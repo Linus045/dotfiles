6be060ec923cbb8c5b99004baed3652dd759285c
Linus045 <lwillms88@gmail.com>
2024-06-28 16:01:28 +0200

    feat(nvim): Show hex-vim command when hex editor opens automatically

--- nvim/.config/nvim/lua/plugins/text_utils/init.lua
+++ nvim/.config/nvim/lua/plugins/text_utils/init.lua
@@ -42,7 +42,27 @@ return {
 	{
 		"RaafatTurki/hex.nvim",
 		config = function()
-			require 'hex'.setup()
+			local hex = require "hex"
+			local original_is_file_binary_pre_read = hex.cfg.is_file_binary_pre_read
+			local original_is_file_binary_post_read = hex.cfg.is_file_binary_post_read
+
+			require 'hex'.setup {
+				is_file_binary_pre_read = function()
+					local is_binary = original_is_file_binary_pre_read()
+					if is_binary then
+						vim.notify("[hex.nvim] Use :HexToggle to switch between hex and text view")
+					end
+					return is_binary
+				end,
+
+				is_file_binary_post_read = function()
+					local is_binary = original_is_file_binary_post_read()
+					if is_binary then
+						vim.notify("[hex.nvim] Use :HexToggle to switch between hex and text view")
+					end
+					return is_binary
+				end,
+			}
 		end
 	},
 	require("plugins.text_utils.vim-glsl"),
