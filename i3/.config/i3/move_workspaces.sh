55fae0331746c426c0438031b4f7320119e6e0c9
Linus045 <lwillms88@gmail.com>
2024-09-04 13:40:46 +0200

    fix(i3): fixes workspaces setup

--- i3/.config/i3/move_workspaces.sh
+++ i3/.config/i3/move_workspaces.sh
@@ -8,21 +8,26 @@ current_workspace=$(i3-msg -t get_workspaces | jq '.[] | select(.focused).num')
 # move workspaces to correct monitor
 i3-msg "workspace 1, move workspace to output HDMI-0"
 i3-msg "workspace 2, move workspace to output HDMI-0"
-i3-msg "workspace 3, move workspace to output DP-1"
-i3-msg "workspace 4, move workspace to output DP-1"
-i3-msg "workspace 5, move workspace to output DP-1"
-i3-msg "workspace 6, move workspace to output DP-1"
-i3-msg "workspace 7, move workspace to output DP-1"
-i3-msg "workspace 8, move workspace to output DP-1"
-i3-msg "workspace 9, move workspace to output DP-1"
+i3-msg "workspace 3, move workspace to output DP-5"
+i3-msg "workspace 4, move workspace to output DP-5"
+i3-msg "workspace 5, move workspace to output DP-5"
+i3-msg "workspace 6, move workspace to output DP-5"
+i3-msg "workspace 7, move workspace to output DP-5"
+i3-msg "workspace 8, move workspace to output DP-5"
+i3-msg "workspace 9, move workspace to output DP-5"
 
 
 
 # for_window [ class="Zathura" workspace="1" Title="/home/linus/dev/rust_seminar/.*\.pdf" ] floating enable, resize set 830 1397, move position 3175 34;
 # for_window [ class="kitty" workspace="1" Title="tmuxstart" ] floating enable, move position 1929 34, resize set 1240 1397;
-i3-msg '[ class="firefox" Title="Linus045 - Chat - Twitch — Mozilla Firefox" ] fullscreen disable, sticky enable, border none, floating enable, move position 4011px 34, resize set 300px 1145px;'
-i3-msg '[ class="kitty" workspace="1" Title="tmuxstart" ] floating enable, move position 1929 34, resize set 2070 1397;'
+i3-msg '[ class="kitty" workspace="1" Title="tmuxstart" ] workspace 1, floating enable, move position 1929 34, resize set 2070 1397;'
 i3-msg '[ class="firefox" workspace="2" Title="^(?!Linus045 - Chat - Twitch — Mozilla Firefox)" ] floating enable, move position 1929 34, resize set 2070 1397;'
+
+# with camera
+#i3-msg '[ class="firefox" Title="Linus045 - Chat - Twitch — Mozilla Firefox" ] fullscreen disable, sticky enable, border none, floating enable, move position 4011px 34, resize set 300px 1145px;'
+
+# without camera
+i3-msg '[ class="firefox" Title="Linus045 - Chat - Twitch — Mozilla Firefox" ] fullscreen disable, sticky enable, border none, floating enable, move position 4011px 34, resize set 300px 1397px;'
 
 
 # move back to workspace
