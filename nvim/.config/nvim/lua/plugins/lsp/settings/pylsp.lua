b91d4104c69e865f0b339f6aa06a03498fb23000
Linus045 <lwillms88@gmail.com>
2024-10-25 13:40:36 +0200

    fix(nvim): fixes linting for pylsp

--- nvim/.config/nvim/lua/plugins/lsp/settings/pylsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp/settings/pylsp.lua
@@ -2,16 +2,24 @@ return {
 	settings = {
 		pylsp = {
 			plugins = {
-				pylint = { enabled = false },
-				pyflakes = { enabled = false },
+				pylint = {
+					enabled = true,
+					-- "C0116", --Missing function or method docstring []
+					-- "C0103", -- [invalid-name] Constant name "n" doesn't conform to UPPER_CASE naming style []
+					args = { '--ignore=C0116,C0103', '-' },
+				},
+				pyflakes = { enabled = true },
 				pycodestyle = {
-					enabled = false,
-					-- ignore = {
-					-- 	"E501", -- line too long (82 > 79 characters)
-					-- 	"E3", -- expected 2 blank lines, found 0
-					-- 	"E266", -- too many leading ‘#’ for block comment
-					-- 	"E228", -- missing whitespace around modulo operator
-					-- }
+					enabled = true,
+					ignore = {
+						"E501", -- line too long (82 > 79 characters)
+						"E3", -- expected 2 blank lines, found 0
+						"E266", -- too many leading ‘#’ for block comment
+						"E228", -- missing whitespace around modulo operator
+						"E226", -- E226 missing whitespace around arithmetic operator
+						"W504", -- W504 line break after binary operator
+						"W503", -- line break before binary operator
+					}
 				},
 			},
 		},
