b6d6b047102f706df0d2e3b01225cff9bd3d3c13
Linus045 <lwillms88@gmail.com>
2025-03-09 22:19:57 +0100

    feat(nvim): Adds formatting rule for glsl using clang-format

--- nvim/.config/nvim/lua/plugins/lsp/mason_nvim.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason_nvim.lua
@@ -228,7 +228,17 @@ return {
 							stdin = true,
 						}
 					end
-				}
+				},
+				glsl = function()
+					local util = require "formatter.util"
+					return {
+						exe = "clang-format",
+						args = {
+							util.escape_path(util.get_current_buffer_file_path()),
+						},
+						stdin = true,
+					}
+				end
 			}
 		})
 
