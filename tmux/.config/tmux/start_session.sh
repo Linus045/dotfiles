bc3dc52d5178a30d2ca4013cf5749ddea53e4f8c
Linus045 <lwillms88@gmail.com>
2025-07-27 17:00:13 +0200

    fix: fixes tmuxstart command

--- tmux/.config/tmux/start_session.sh
+++ tmux/.config/tmux/start_session.sh
@@ -6,7 +6,7 @@ if [ -n "$connected" ]; then
 fi
 
 tmux new-session -s "dev" -d -n "terminal"
-tmux move-window -t 3
+tmux move-window -t 2
 
 # nvim in project directory
 # tmux new-window -t dev -da -n "dev"
@@ -15,17 +15,12 @@ tmux move-window -t 3
 # tmux move-window -t 2
 
 # nvim in project directory
-tmux new-window -da -c "$vulkan_dir" -n "vulkan" -t dev
-tmux select-window -t dev:vulkan
-tmux send-keys -t dev:vulkan "z vulkan_experiments && nvim" Enter
+tmux new-window -da -c "$PWD" -n "nvim" -t dev
+tmux select-window -t dev:nvim
+tmux send-keys -t dev:nvim "nvim" Enter
 tmux move-window -t 1
 
 # bachelorarbeit
-tmux new-window -da -c "$bachelorarbeit_dir" -n "bachelorarbeit" -t dev
-tmux select-window -t dev:bachelorarbeit
-tmux send-keys -t dev:bachelorarbeit "z bachelorarbeit && nvim" Enter
-tmux move-window -t 2
-
 # dotfiles
 # tmux new-window -t dev -da -n "dotfiles" "cd ~/dotfiles && nvim"
 # tmux select-window -t dev:dotfiles
@@ -37,7 +32,7 @@ tmux move-window -t 2
 # tmux send-keys -t dev:nvim_journal "z nvim_journal && nvim" Enter
 # tmux move-window -t 9
 
-tmux select-window -t dev:vulkan
+tmux select-window -t dev:nvim
 
 tmux attach -t dev
 # tmux kill-session -t dev
