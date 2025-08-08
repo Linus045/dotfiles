23d8d4260e1bef0723ad46f3cd5ca5d3f5cb5826
Linus045 <linuswillms00@gmail.com>
2025-08-09 01:46:16 +0200

    fix(nvim): adds cmake formatter

--- nvim/.config/nvim/lua/plugins/lsp/mason_nvim.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason_nvim.lua
@@ -238,6 +238,16 @@ return {
 						},
 						stdin = true,
 					}
+				end,
+				cmake = function()
+					local util = require "formatter.util"
+					return {
+						exe = "cmake-format",
+						args = {
+							util.escape_path(util.get_current_buffer_file_path()),
+						},
+						stdin = true,
+					}
 				end
 			}
 		})
