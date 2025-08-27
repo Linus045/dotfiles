e4206790ecdce2a6fb86cee9998f0bf17d417070
Linus045 <lwillms88@gmail.com>
2025-08-27 02:49:25 +0200

    fix(nvim): fixes vim-cmake default config

--- nvim/.config/nvim/lua/plugins/special/init.lua
+++ nvim/.config/nvim/lua/plugins/special/init.lua
@@ -5,7 +5,10 @@ return {
 	{
 		"cdelledonne/vim-cmake",
 		config = function()
-			vim.g.cmake_build_dir_location = "build"
+			-- use ':CMakeGenerate' as 'build' is already defined as default config
+			vim.g.cmake_build_dir_location = "."
+			vim.g.cmake_default_config = "build"
+			vim.g.cmake_generate_options = { "-D CMAKE_EXPORT_COMPILE_COMMANDS=ON" }
 		end
 	},
 	-- Tetris
